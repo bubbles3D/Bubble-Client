155e711f67d0e1c4668ceb153ac934433229020c
mathieu <sabourin.mathieu@free.fr>
2012-01-31 21:04:08 +0100

    Updated player with view point.

--- player.cpp
+++ player.cpp
@@ -1,12 +1,27 @@
 #include "player.h"
 
 
-Player::Player(QString name, float x, float y, float z)
+Player::Player(QString name, float x, float y, float z,
+               float vx, float vy, float vz)
 {
     this->name = name;
     this->x = x;
     this->y = y;
     this->z = z;
+    this->vx = vx;
+    this->vy = vy;
+    this->vz = vz;
+}
+
+Player::Player(QVariantMap map)
+{
+    name = map["name"].toString();
+    x = map["x"].toInt();
+    y = map["y"].toInt();
+    z = map["z"].toInt();
+    vx = map["vx"].toInt();
+    vy = map["vy"].toInt();
+    vz = map["vz"].toInt();
 }
 
 QString Player::toString()
@@ -32,3 +47,16 @@ float Player::getY(){
 float Player::getZ(){
     return z;
 }
+
+float Player::getVx(){
+    return vx;
+}
+
+float Player::getVy(){
+    return vy;
+}
+
+float Player::getVz(){
+    return vz;
+}
+
--- player.h
+++ player.h
@@ -3,20 +3,27 @@
 
 #include <string>
 #include <QString>
+#include <QVariant>
 
 class Player
 {
 public:
-    Player(QString name="player", float x=0, float y=0, float z=0);
+    Player(QString name="player", float x=0, float y=0, float z=0,
+           float vx=0, float vy=0, float vz=0);
+    Player(QVariantMap dict);
     QString toString();
     float getX();
     float getY();
     float getZ();
+    float getVx();
+    float getVy();
+    float getVz();
     QString getName();
 
 private:
     QString name;
     float x, y, z;
+    float vx, vy, vz;
 };
 
 #endif // PLAYER_H
