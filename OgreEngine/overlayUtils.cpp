14028d444c0d9283f837127c8eecc136976bd656
michael fagno <michael.fagno@free.fr>
2012-03-30 12:39:20 +0200

    Useless switch case removed

--- OgreEngine/overlayUtils.cpp
+++ OgreEngine/overlayUtils.cpp
@@ -5,28 +5,14 @@ void OverlayUtils::setScaleKeepingCenter(Ogre::OverlayContainer * container, flo
      Ogre::Real oldWidth = container->getWidth();
      Ogre::Real oldLeft = container->getLeft();
      Ogre::Real oldTop = container->getTop();
-     Ogre::GuiHorizontalAlignment alignementH = container->getHorizontalAlignment();
-     Ogre::GuiVerticalAlignment alignementV = container->getVerticalAlignment();
      Ogre::GuiMetricsMode metricMode = container->getMetricsMode();
 
      switch(metricMode){
      case Ogre::GMM_PIXELS:
-         switch(alignementH){
-         case Ogre::GHA_RIGHT:
-             switch(alignementV){
-             case Ogre::GVA_TOP:
-                 container->setWidth(width);
-                 container->setHeight(height);
-                 container->setTop(oldTop + (oldHeight - height)/2);
-                 container->setLeft(oldLeft + (oldWidth - width)/2);
-                 break;
-             default:
-                 break;
-             }
-             break;
-             default:
-             break;
-         }
+             container->setWidth(width);
+             container->setHeight(height);
+             container->setTop(oldTop + (oldHeight - height)/2);
+             container->setLeft(oldLeft + (oldWidth - width)/2);
          break;
          default:
          break;
--- OgreEngine/overlayUtils.h
+++ OgreEngine/overlayUtils.h
@@ -6,7 +6,7 @@ class OverlayUtils{
 public:
     /*
     * set the scale of the container to value keeping the center of the container
-    * Note: currently it's only implemented for pixel mode when the panel is on the top right (cf alignment)
+    * Note: currently it's only implemented for pixel mode
     * If it's not the case: do nothing
     */
     static void setScaleKeepingCenter(Ogre::OverlayContainer * container, float height, float width);
