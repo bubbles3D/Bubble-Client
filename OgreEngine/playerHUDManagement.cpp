980f8114f46b1b6891b5d450eccdf3cd001f5690
Stanislas Signoud <stan@stanisoft.net>
2012-04-26 01:01:40 +0200

    Fixing (again) time left formatting.

--- OgreEngine/playerHUDManagement.cpp
+++ OgreEngine/playerHUDManagement.cpp
@@ -151,8 +151,7 @@ void PlayerHUDManagement::updateHUD(float timeSinceLastFrame){
     Model* mod = Model::getInstance();
     int min =(int) mod->getRemainingTime()/60;
     int sec = (int) mod->getRemainingTime() % 60;
-    QString time = QString::number(min) + ":" + QString::number(sec);
-            // QString::sprintf("%i:%02i", min, sec); // Format => M:SS
+    QString time = QString("%1:%2").arg(min).arg(sec, 2, 10, QChar('0')); // Format => M:SS
     setTime(time);
 }
 
